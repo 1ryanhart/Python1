5a518f31da3a687f347cffeadc12934f9ef5b182
Ryan-Hart <1ryanhart@gmail.com>
2022-04-26 10:03:29 +0100

    all rubric complete

---
+++ database.py
@@ -0,0 +1,111 @@
+"""A database encapsulating collections of near-Earth objects and their close approaches.
+
+A `NEODatabase` holds an interconnected data set of NEOs and close approaches.
+It provides methods to fetch an NEO by primary designation or by name, as well
+as a method to query the set of close approaches that match a collection of
+user-specified criteria.
+
+Under normal circumstances, the main module creates one NEODatabase from the
+data on NEOs and close approaches extracted by `extract.load_neos` and
+`extract.load_approaches`.
+
+You'll edit this file in Tasks 2 and 3.
+"""
+
+
+class NEODatabase:
+    """A database of near-Earth objects and their close approaches.
+
+    A `NEODatabase` contains a collection of NEOs and a collection of close
+    approaches. It additionally maintains a few auxiliary data structures to
+    help fetch NEOs by primary designation or by name and to help speed up
+    querying for close approaches that match criteria.
+    """
+    def __init__(self, neos, approaches):
+        """Create a new `NEODatabase`.
+
+        As a precondition, this constructor assumes that the collections of NEOs
+        and close approaches haven't yet been linked - that is, the
+        `.approaches` attribute of each `NearEarthObject` resolves to an empty
+        collection, and the `.neo` attribute of each `CloseApproach` is None.
+
+        However, each `CloseApproach` has an attribute (`._designation`) that
+        matches the `.designation` attribute of the corresponding NEO. This
+        constructor modifies the supplied NEOs and close approaches to link them
+        together - after it's done, the `.approaches` attribute of each NEO has
+        a collection of that NEO's close approaches, and the `.neo` attribute of
+        each close approach references the appropriate NEO.
+
+        :param neos: A collection of `NearEarthObject`s.
+        :param approaches: A collection of `CloseApproach`es.
+        """
+        self._neos = neos
+        self._approaches = approaches
+
+        #Create a dictionary of neos with their designation as the key, to enable fast lookup
+        self.neos_dict={}
+        for neo in neos:
+            self.neos_dict[neo.designation] = neo
+
+        for approach in approaches:
+            associated_neo = self.neos_dict.get(approach._designation) #find the corresponding neo object based on its designation
+            approach.neo = associated_neo   #associate this neo object to the approach.neo
+            associated_neo.approaches.append(approach)  #append this approach to the neo's approaches list
+
+
+    def get_neo_by_designation(self, designation):
+        """Find and return an NEO by its primary designation.
+
+        If no match is found, return `None` instead.
+
+        Each NEO in the data set has a unique primary designation, as a string.
+
+        The matching is exact - check for spelling and capitalization if no
+        match is found.
+
+        :param designation: The primary designation of the NEO to search for.
+        :return: The `NearEarthObject` with the desired primary designation, or `None`.
+        """
+        return self.neos_dict.get(designation, None)
+
+    def get_neo_by_name(self, name):
+        """Find and return an NEO by its name.
+
+        If no match is found, return `None` instead.
+
+        Not every NEO in the data set has a name. No NEOs are associated with
+        the empty string nor with the `None` singleton.
+
+        The matching is exact - check for spelling and capitalization if no
+        match is found.
+
+        :param name: The name, as a string, of the NEO to search for.
+        :return: The `NearEarthObject` with the desired name, or `None`.
+        """
+        for neo in self._neos:
+            if neo.name == name:
+                return neo      
+        return None
+
+    def query(self, filters=()):
+        """Query close approaches to generate those that match a collection of filters.
+
+        This generates a stream of `CloseApproach` objects that match all of the
+        provided filters.
+
+        If no arguments are provided, generate all known close approaches.
+
+        The `CloseApproach` objects are generated in internal order, which isn't
+        guaranteed to be sorted meaningfully, although is often sorted by time.
+
+        :param filters: A collection of filters capturing user-specified criteria.
+        :return: A stream of matching `CloseApproach` objects.
+        """
+        for approach in self._approaches:
+            doesPass = True
+            for filter in filters:
+                if not filter(approach):
+                    doesPass = False
+                    break
+            if doesPass:
+                yield approach
---
+++ models.py
@@ -0,0 +1,143 @@
+"""Represent models for near-Earth objects and their close approaches.
+
+The `NearEarthObject` class represents a near-Earth object. Each has a unique
+primary designation, an optional unique name, an optional diameter, and a flag
+for whether the object is potentially hazardous.
+
+The `CloseApproach` class represents a close approach to Earth by an NEO. Each
+has an approach datetime, a nominal approach distance, and a relative approach
+velocity.
+
+A `NearEarthObject` maintains a collection of its close approaches, and a
+`CloseApproach` maintains a reference to its NEO.
+
+The functions that construct these objects use information extracted from the
+data files from NASA, so these objects should be able to handle all of the
+quirks of the data set, such as missing names and unknown diameters.
+
+You'll edit this file in Task 1.
+"""
+from helpers import cd_to_datetime, datetime_to_str
+
+
+class NearEarthObject:
+    """A near-Earth object (NEO).
+
+    An NEO encapsulates semantic and physical parameters about the object, such
+    as its primary designation (required, unique), IAU name (optional), diameter
+    in kilometers (optional - sometimes unknown), and whether it's marked as
+    potentially hazardous to Earth.
+
+    A `NearEarthObject` also maintains a collection of its close approaches -
+    initialized to an empty collection, but eventually populated in the
+    `NEODatabase` constructor.
+    """
+    def __init__(self, **info):
+        """Create a new `NearEarthObject`.
+
+        :param info: A dictionary of excess keyword arguments supplied to the constructor
+        """
+        self.designation = info.get('pdes')
+
+        if not info.get('name'):
+            self.name = None
+        else:
+            self.name = info.get('name')
+
+        if not info.get('diameter'):
+            self.diameter = float('nan')
+        else:
+            self.diameter = float(info.get('diameter'))
+ 
+        self.hazardous = info.get('pha', False)
+        self.hazardous = True if self.hazardous=='Y' else False
+
+        # Create an empty initial collection of linked approaches.
+        self.approaches = []
+
+    @property
+    def fullname(self):
+        """Return a representation of the full name of this NEO."""
+        if self.name is None:
+            return_name = ''
+        else:
+            return_name = f" ({self.name})"
+        return f"{self.designation}{return_name}"
+
+    def __str__(self):
+        """Return `str(self)`."""
+        is_or_not = '' if self.hazardous else ' not'
+        return f"NEO {self.fullname} has a diameter of {self.diameter:.3f} km and is{is_or_not} potentially hazardous"
+
+    def __repr__(self):
+        """Return `repr(self)`, a computer-readable string representation of this object."""
+        return f"NearEarthObject(designation={self.designation!r}, name={self.name!r}, " \
+               f"diameter={self.diameter:.3f}, hazardous={self.hazardous!r})"
+
+    def serialise(self):
+        """Return a serialised object to aid exporting to cvs and JSON"""
+        return ({
+            'designation': self.designation, 
+            'name': self.name, 
+            'diameter_km': self.diameter, 
+            'potentially_hazardous': self.hazardous
+            })
+
+
+class CloseApproach:
+    """A close approach to Earth by an NEO.
+
+    A `CloseApproach` encapsulates information about the NEO's close approach to
+    Earth, such as the date and time (in UTC) of closest approach, the nominal
+    approach distance in astronomical units, and the relative approach velocity
+    in kilometers per second.
+
+    A `CloseApproach` also maintains a reference to its `NearEarthObject` -
+    initially, this information (the NEO's primary designation) is saved in a
+    private attribute, but the referenced NEO is eventually replaced in the
+    `NEODatabase` constructor.
+    """
+    def __init__(self, **info):
+        """Create a new `CloseApproach`.
+
+        :param info: A dictionary of excess keyword arguments supplied to the constructor.
+        """
+        self._designation = info.get('des')
+        self.time = cd_to_datetime(info.get('cd'))
+        self.distance = 0.0 if not float(info.get('dist')) else float(info.get('dist'))
+        self.velocity = 0.0 if not float(info.get('v_rel')) else float(info.get('v_rel'))
+
+        # Create an attribute for the referenced NEO, originally None.
+        self.neo = None
+
+    @property
+    def time_str(self):
+        """Return a formatted representation of this `CloseApproach`'s approach time.
+
+        The value in `self.time` should be a Python `datetime` object. While a
+        `datetime` object has a string representation, the default representation
+        includes seconds - significant figures that don't exist in our input
+        data set.
+
+        The `datetime_to_str` method converts a `datetime` object to a
+        formatted string that can be used in human-readable representations and
+        in serialization to CSV and JSON files.
+        """
+        return datetime_to_str(self.time)
+
+    def __str__(self):
+        """Return `str(self)`."""
+        return f"At {self.time_str}, '{self.neo.fullname}' approaches Earth at a distance of " \
+               f"{self.distance:.2f} au and a velocity of {self.velocity:.2f} km/s."
+
+    def __repr__(self):
+        """Return `repr(self)`, a computer-readable string representation of this object."""
+        return f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f}, " \
+               f"velocity={self.velocity:.2f}, neo={self.neo!r})"
+
+    def serialise(self):
+        return({
+                'datetime_utc': datetime_to_str(self.time), 
+                'distance_au': self.distance, 
+                'velocity_km_s': self.velocity, 
+                })
